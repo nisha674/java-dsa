87f4ae17f781875780132d57f1c4ed578becb3b4
Nisha Ingole <ingolenisha9@gmail.com>
2025-08-04 21:11:34 +0530

    Day 3

---
+++ Array/TrappingRainwater.java
@@ -0,0 +1,34 @@
+public class TrappingRainwater {
+
+    public static int trappedRainwater(int[] height){
+        int n = height.length;
+        // leftMax boundary
+        int[] leftMax = new int[n];
+        leftMax[0] = height[0];
+        for(int i = 1; i< height.length; i++){
+            leftMax[i] = Math.max(height[i] , leftMax[i-1]);
+        }
+
+        // rightMax boundaray
+        int[] rightMax = new int[n];
+        rightMax[n-1] = height[n-1];
+        for(int i = n-2; i>=0; i--){
+            rightMax[i] = Math.max(height[i] , rightMax[i+1]);
+        }
+
+
+        int trappedRainwater = 0;
+        for(int i = 0; i< n; i++){
+            int waterLevel = Math.min(leftMax[i] , rightMax[i]);
+            trappedRainwater += waterLevel-height[i];
+        }
+
+        return trappedRainwater;
+    }
+
+
+    public static void main(String[] args){
+        int[] height = {4 , 2 , 0 , 6 , 3 , 2 , 5};
+        System.out.println(trappedRainwater(height));
+    }
+}
